c5b14c5ad005b53fd2fe1ef6b61e4b14618bed35
areasplash <areasplash@dgu.ac.kr>
2024-03-25 13:18:11 +0900

    2024-03-25 OSSP 개인과제#1

--- OSSP_hw_2/main.cpp
+++ OSSP_hw_2/main.cpp
@@ -1,7 +1,18 @@
-#include <iostream>
+﻿#include <iostream>
+#include <string>
 using namespace std;
 
 int main() {
+	string name;
+	string code;
 
+	cout << "이름을 입력하세요: ";
+	cin >> name;
+	cout << "학번을 입력하세요: ";
+	cin >> code;
+
+	cout << "<출력>" << endl;
+	cout << "이름: " << name << endl;
+	cout << "학번: " << code << endl;
 	return 0;
 }
